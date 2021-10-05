03cb8f4f59f5fc66ed69b200b2497f69e480bccb
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-10-05 21:31:41 +0900

    Add Basic delivery App

---
+++ Assignment6_Delivery/SSACAssignment6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSACAssignment6
+//
+//  Created by 최광호 on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
