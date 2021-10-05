0f2eebdfa0d91c04db17872c8fcbbedb1176c219
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-10-05 17:49:14 +0900

    test

---
+++ FirstTest/FirstTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstTest
+//
+//  Created by 최광호 on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("test")
+    }
+
+
+}
+
