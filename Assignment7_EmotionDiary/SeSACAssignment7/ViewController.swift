c1a4c00f15683169537c8ea850ab217e29e00720
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-10-07 02:24:29 +0900

    Add remove all & Fix UserDefault

--- Assignment7_EmotionDiary/SeSACAssignment7/ViewController.swift
+++ Assignment7_EmotionDiary/SeSACAssignment7/ViewController.swift
@@ -32,92 +32,85 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let initNumber = UserDefaults.standard.integer(forKey: "initNumber")
-        lbl1.text = "행복해 \(initNumber)"
+        let numberA = UserDefaults.standard.integer(forKey: "행복해")
+        let numberB = UserDefaults.standard.integer(forKey: "사랑해")
+        let numberC = UserDefaults.standard.integer(forKey: "좋아해")
+        let numberD = UserDefaults.standard.integer(forKey: "당황해")
+        let numberE = UserDefaults.standard.integer(forKey: "속상해")
+        let numberF = UserDefaults.standard.integer(forKey: "우울해")
+        let numberG = UserDefaults.standard.integer(forKey: "심심해")
+        let numberH = UserDefaults.standard.integer(forKey: "행복해")
+        let numberI = UserDefaults.standard.integer(forKey: "몰라요")
         
-        let TwoNumber = UserDefaults.standard.integer(forKey: "2Number")
-        lbl2.text = "사랑해 \(TwoNumber)"
+        lbl1.text = "행복해 \(numberA)"
+        lbl2.text = "사랑해 \(numberB)"
+        lbl3.text = "좋아해 \(numberC)"
+        lbl4.text = "당황해 \(numberD)"
+        lbl5.text = "속상해 \(numberE)"
+        lbl6.text = "우울해 \(numberF)"
+        lbl7.text = "심심해 \(numberG)"
+        lbl8.text = "행복해 \(numberH)"
+        lbl9.text = "몰라요 \(numberI)"
         
-        let ThreeNumber = UserDefaults.standard.integer(forKey: "3Number")
-        lbl3.text = "좋아해 \(ThreeNumber)"
-        
-        lbl4.text = "속상해"
-        lbl5.text = "우울해"
-        lbl6.text = "아이고"
-        lbl7.text = "조금만"
-        lbl8.text = "더더더"
-        lbl9.text = "생각해"
-    
-        
-//
-//        let emotionCounting = [1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"]
-//
-//
-//        for (key: value) in emotionCounting {
-//            print("\(value)")
-//        }
+        // 추가 수정 예정
     }
+    
+    @IBAction func deleteUsersDefault(_ sender: UIButton) {
+        // 전체 삭제
+        let domain = Bundle.main.bundleIdentifier!
+        UserDefaults.standard.removePersistentDomain(forName: domain)
+        UserDefaults.standard.synchronize()
+    }
+    
+    @IBAction func checkingEmotion4(_ sender: UIButton) {
+        let ud = UserDefaults.standard
 
-    @IBAction func checkingEmotion1(_ sender: UIButton) {
-        let initNumber = UserDefaults.standard.integer(forKey: "initNumber")
-        UserDefaults.standard.set(initNumber + 1, forKey: "initNumber")
-        
-        let updateNumber = UserDefaults.standard.integer(forKey: "initNumber")
-        lbl1.text = "행복해 \(updateNumber)"
-        
-//        let emotionCounting: [UIButton: String] = [btn1: "One", btn2: "Two", btn3: "Three", btn4: "Four", btn5: "Five", btn6: "Six", btn7: "Seven", btn8: "Eight", btn9: "Nine"]
-//
-//        for (UIButton, String) in emotionCounting {
-//            let iNumber = UserDefaults.standard.integer(forKey: "\(String)")
-//            UserDefaults.standard.set(iNumber + 1, forKey: "\(String)")
-//            let uNumber = UserDefaults.standard.integer(forKey: "\(String)")
-//            lbl2.text = "행복해 \(updateNumber)"
+        if btn1.isTouchInside {
+            let numberA = ud.integer(forKey: "행복해")
+            ud.set(numberA + 1, forKey: "행복해")
+            let numberAA = ud.integer(forKey: "행복해")
+            lbl1.text = "행복해 \(numberAA)"
+        } else if btn2.isTouchInside {
+            let numberB = ud.integer(forKey: "사랑해")
+            ud.set(numberB + 1, forKey: "사랑해")
+            let numberBB = ud.integer(forKey: "사랑해")
+            lbl2.text = "사랑해 \(numberBB)"
+        } else if btn3.isTouchInside {
+            let numberC = ud.integer(forKey: "좋아해")
+            ud.set(numberC + 1, forKey: "좋아해")
+            let numberCC = ud.integer(forKey: "좋아해")
+            lbl3.text = "좋아해 \(numberCC)"
+        } else if btn4.isTouchInside {
+            let numberD = ud.integer(forKey: "당황해")
+            ud.set(numberD + 1, forKey: "당황해")
+            let numberDD = ud.integer(forKey: "당황해")
+            lbl4.text = "당황해 \(numberDD)"
+        } else if btn5.isTouchInside {
+            let numberE = ud.integer(forKey: "속상해")
+            ud.set(numberE + 1, forKey: "속상해")
+            let numberEE = ud.integer(forKey: "속상해")
+            lbl5.text = "속상해 \(numberEE)"
+        } else if btn6.isTouchInside {
+            let numberF = ud.integer(forKey: "우울해")
+            ud.set(numberF + 1, forKey: "우울해")
+            let numberFF = ud.integer(forKey: "우울해")
+            lbl6.text = "우울해 \(numberFF)"
+        } else if btn7.isTouchInside {
+            let numberG = ud.integer(forKey: "심심해")
+            ud.set(numberG + 1, forKey: "심심해")
+            let numberGG = ud.integer(forKey: "심심해")
+            lbl7.text = "심심해 \(numberGG)"
+        } else if btn8.isTouchInside {
+            let numberH = ud.integer(forKey: "행복해")
+            ud.set(numberH + 1, forKey: "행복해")
+            let numberHH = ud.integer(forKey: "행복해")
+            lbl8.text = "행복해 \(numberHH)"
+        } else if btn9.isTouchInside {
+            let numberI = ud.integer(forKey: "몰라요")
+            ud.set(numberI + 1, forKey: "몰라요")
+            let numberII = ud.integer(forKey: "몰라요")
+            lbl9.text = "몰라요 \(numberII)"
+        }
     }
-    
-    @IBAction func checkingEmotion2(_ sender: UIButton) {
-        // 이렇게 노가다는 아닌 듯...
-        let twoNumber = UserDefaults.standard.integer(forKey: "2Number")
-        UserDefaults.standard.set(twoNumber + 1, forKey: "2Number")
-        
-        let u2Number = UserDefaults.standard.integer(forKey: "2Number")
-        lbl2.text = "사랑해 \(u2Number)"
-    }
-    
-    @IBAction func checkingEmotion3(_ sender: UIButton) {
-        // 아 정말 이건 아닌 듯 ...
-        // 오기 생겨서 혼자 한다 ...
-        let threeNumber = UserDefaults.standard.integer(forKey: "3Number")
-        UserDefaults.standard.set(threeNumber + 1, forKey: "3Number")
-        
-        let u3Number = UserDefaults.standard.integer(forKey: "3Number")
-        lbl3.text = "좋아해 \(u3Number)"
-    }
-    
     
 }
-    
-    /*
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // 출첵
-        let number = UserDefaults.standard.integer(forKey: "number")
-        numberlbl.text = "\(number)"
-    }
-    
-    // 출첵 2 - 출석체크 버튼 누를 때의 기능
-    // 10번 출첵 -> 3번 누르면 -> 13번 저장
-    @IBAction func checkButtonClicked(_ sender: UIButton) {
-        // 출첵 3 - 기존 출첵된 숫자 가져오기 (10번)
-        let number = UserDefaults.standard.integer(forKey: "number")
-        
-        // 출첵 4 - 기존 출첵된 숫자에서 1을 더한 값을 새롭게 number 키에 저장
-        UserDefaults.standard.set(number + 1, forKey: "number")
-        
-        // 출첵 5 - lbl에 보여지고 있는 값 업데이트
-        let updateNumber = UserDefaults.standard.integer(forKey: "number")
-        numberlbl.text = "\(updateNumber)"
-    }
-     */
-
-    
-
