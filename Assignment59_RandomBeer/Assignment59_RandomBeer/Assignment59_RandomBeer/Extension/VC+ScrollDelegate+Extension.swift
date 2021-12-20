fd9bf43d8619bee1652ace0d5925967c3001c3d4
Lautner-kwangho <wooree2716@khcu.ac.kr>
2021-12-21 04:28:55 +0900

    금일은 여기까지 (미구현)

--- Assignment59_RandomBeer/Assignment59_RandomBeer/Assignment59_RandomBeer/Extension/VC+ScrollDelegate+Extension.swift
+++ Assignment59_RandomBeer/Assignment59_RandomBeer/Assignment59_RandomBeer/Extension/VC+ScrollDelegate+Extension.swift
@@ -12,6 +12,23 @@ extension ViewController: UIScrollViewDelegate {
         
 //        guard let header = detailTableView.tableHeaderView as self.headerView else {return}
 //        header.scroll
-
+        
+//        headerView.snp.makeConstraints { make in
+//            make.height.equalTo(300).multipliedBy(0.5).offset(-scrollView.contentOffset.y)
+//        }
+        
+//        .multipliedBy(0.5).offset(-scrollView.contentOffset.y)
+        let y = -scrollView.contentOffset.y
+        
+        if y > 0 {
+            headerView.snp.makeConstraints { make in
+                make.top.equalTo(view)
+                make.width.equalTo(view).multipliedBy(y)
+                make.height.equalTo(y).multipliedBy(y)
+//                make.bottom.equalTo(detailTableView.snp.top)
+            }
+        }
+        print(y)
     }
+
 }
